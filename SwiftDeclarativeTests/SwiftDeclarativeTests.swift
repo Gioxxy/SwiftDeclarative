40bd84eb28973e12a1fda1b5da48df51f65ce5bc
Gioxxy <gionatan99@gmail.com>
2020-08-25 17:55:37 +0200

    Fix gitignore

---
+++ SwiftDeclarativeTests/SwiftDeclarativeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftDeclarativeTests.swift
+//  SwiftDeclarativeTests
+//
+//  Created by Gionatan Cernusco on 25/08/2020.
+//  Copyright © 2020 Gionatan Cernusco. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftDeclarative
+
+class SwiftDeclarativeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
